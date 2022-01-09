f3c1c3cbcd8a1569524455d7c4abf7b34e05eeb4
kaingadeaniruddha21 <93026301+kaingadeaniruddha21@users.noreply.github.com>
2022-01-09 18:35:19 +0530

    Exercise_Single_Level_Inheritance

---
+++ single_level_Ineheritance/Employee_Management_System.java
@@ -0,0 +1,34 @@
+package single_level_Ineheritance;
+
+public class Employee_Management_System {
+	
+	
+		public static void main(String[] args) {
+		
+		/*
+		 * //Object of Employee Employee e = new Employee();
+		 * e.readEmployeeInformation(); e.viewEmployeeInformation();
+		 */
+		
+		/* in inheritance we can create an object to subclass to use the properties
+		 * of superclass ,JVM copies the properties of super class in to subclass when we
+		 * use extends keyword 
+		 */
+		//Object of the programmer
+		
+		  Programmer prg = new Programmer(); //prg.readEmployeeInformation();
+		  prg.readProgrammerInformation(); //prg.viewEmployeeInformation();
+		  prg.viewProgrammerInformation();
+		 
+		
+
+	}
+}
+
+
+
+//import inheritanceandpolymorphism.multilevelinheritanceexample.PythonDeveloper;
+
+
+
+	
---
+++ single_level_Ineheritance/Programmer.java
@@ -0,0 +1,26 @@
+package single_level_Ineheritance;
+
+
+public class Programmer extends Employee{
+	
+	String[] technologies = { "MySql","Java","Spring","Spring Boot","Python"};
+	String projectName;
+	
+	void readProgrammerInformation() {
+		readEmployeeInformation();
+		System.out.println("Enter the project Name");
+		sc.nextLine();
+		projectName = sc.nextLine();
+		
+	}
+	void viewProgrammerInformation() {
+		System.out.println("Programmer Details are ........");
+		viewEmployeeInformation();
+		System.out.println("Project Name " + projectName);
+		System.out.println("Technologies used ..... ");
+		for (String tech : technologies) {
+			System.out.print(tech + "\t");
+		}
+	}
+
+}
